448e4186a8ac474f74a667245f80085853dac295
absentservices0-beep <absentservices0@gmail.com>
2026-01-15 20:20:35 -0600

    Create corrupt.user.js

---
+++ corrupt.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         My Cross-Platform Script
+// @version      1.0.1
+// @match        *https://discord.com/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com
+// @downloadURL  https://raw.githubusercontent.com
+// ==/UserScript==
+
+console.log("Script version 1.0.1 is active.");
